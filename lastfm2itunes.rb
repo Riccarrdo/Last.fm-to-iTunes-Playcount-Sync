b797d9af94e93eeff6fb7cf0436cbdd6e4ff1f46
Erik Peterson <erik@subwindow.com>
2009-07-23 14:35:27 -0400

    Unicode normalization so Bjork will work.

--- lastfm2itunes.rb
+++ lastfm2itunes.rb
@@ -1,3 +1,4 @@
+# coding: UTF-8
 # This is a tiny utility script to update your iTunes Library's played counts to match your last.fm listening data
 #  This is useful if you've had to rebuild your library for any reason.
 #  The utility only updates tracks for which the last.fm play count is greater than the iTunes play count.
@@ -10,9 +11,28 @@ require 'nokogiri' rescue "This script depends on the Nokogiri gem. Please run '
 require 'appscript' rescue "This script depends on the rb-appscript gem. Please run '(sudo) gem install rb-appscript'."
 include Appscript
 
+def filter_name(name)
+  name.force_encoding("utf-8")
+  # This is a hack until I can find out how to do true Unicode normalization in Ruby 1.9
+  #  (The Unicode gem doesn't work in 1.9, String#chars.normalize is gone. WTF to do?)
+  {
+    ['á','à','â','ä','Ä','Â','À','Á','ã','Ã'] => 'a',
+    ['é','è','ê','ë','Ë','Ê','È','É']         => 'e',
+    ['í','ì','î','ï','Ï','Î','Ì','Í']         => 'i',
+    ['ó','ò','ô','ö','Ö','Ô','Ò','Ó','õ','Õ'] => 'o',
+    ['ú','ù','û','ü','Ü','Û','Ù','Ú']         => 'u',
+    ['ñ','Ñ']                                 => 'n',
+    ['ç','Ç']                                 => 'c',
+  }.each do |family, replacement|
+    family.each { |accent| name.gsub!(accent, replacement) }
+  end
+  name.downcase.gsub(/^the /, "").gsub(/ the$/, "").gsub(/[^\w]/, "")
+end
+
 filename = "cached_lastfm_data.rbmarshal"
 begin
   playcounts = Marshal.load(File.read(filename))
+
   puts "Reading cached playcount data from disk"
 rescue
   puts "No cached playcount data, grabbing fresh data from Last.fm"
@@ -26,14 +46,14 @@ rescue
     sleep 0.1
     begin
       Nokogiri::HTML(open("http://ws.audioscrobbler.com/2.0/user/#{username}/weeklytrackchart.xml?from=#{from}&to=#{to}")).search('weeklytrackchart').search('track').each do |track|
-        artist = track.search('artist').first.content.downcase.gsub(/[^\w]/, "").gsub("the", "")
-        name = track.search('name').first.content.downcase.gsub(/[^\w]/, "").gsub("the", "")
+        artist = filter_name(track.search('artist').first.content)
+        name = filter_name(track.search('name').first.content)
         playcounts[artist] ||= {}
         playcounts[artist][name] ||= 0
         playcounts[artist][name] += track.search('playcount').first.content.to_i
       end
-    rescue
-      puts "Error getting listening data for week of #{time.year}-#{time.month}-#{time.day}"
+      #rescue
+      #  puts "Error getting listening data for week of #{time.year}-#{time.month}-#{time.day}"
     end
   end
 
@@ -46,13 +66,13 @@ end
 iTunes = app('iTunes')
 iTunes.tracks.get.each do |track|
   begin
-    artist = playcounts[track.artist.get.downcase.gsub(/[^\w]/, "").gsub("the", "")]
+    artist = playcounts[filter_name(track.artist.get)]
     if artist.nil?
       puts "Couldn't match up #{track.artist.get}"
       next
     end
 
-    playcount = artist[track.name.get.downcase.gsub(/[^\w]/, "").gsub("the", "")]
+    playcount = artist[filter_name(track.name.get)]
     if playcount.nil?
       puts "Couldn't match up #{track.artist.get} - #{track.name.get}"
       next
